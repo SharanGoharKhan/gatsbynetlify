d86a221d3913c555bffa64f54bc8340a010a5c23
Sharan Khan <sharan.gohar@gmail.com>
2021-06-04 15:59:00 +0500

    contact form

--- src/components/Contactus/index.js
+++ src/components/Contactus/index.js
@@ -47,36 +47,33 @@ const useStyles = makeStyles((theme) => ({
 
 const ContactUs = () => {
     const classes = useStyles();
-    // const [params, setParams] = useState({
-    //     username: '',
-    //     email: '',
-    //     message: ''
-    // })
     return (
         <div id="contact" className={classes.container}>
             <Grid container justify="center" direction="row" spacing={5} alignItems="center" >
-                <form name="contact" method="post" data-netlify="true" netlify-honeypot="bot-field">
-                    <input type="hidden" name="form-name" value="contact" />
-                    <Grid item xs={12}>
-                        <Typography className={classes.heading} variant="h3" gutterBottom>
-                            Let's Talk with us
+                <Grid item xs={12}>
+                    <form name="contact" method="post" data-netlify="true" netlify-honeypot="bot-field">
+                        <input type="hidden" name="form-name" value="contact" />
+                        <Grid item xs={12}>
+                            <Typography className={classes.heading} variant="h3" gutterBottom>
+                                Let's Talk with us
                         </Typography>
-                    </Grid>
-                    <Grid item xs={12} md={4}>
-                        <input name="name" type="text" className={classes.textInput} placeholder="Enter your name" />
-                    </Grid>
-                    <Grid item xs={12} md={4}>
-                        <input name="email" type="text" className={classes.textInput} placeholder="Enter your email" />
-                    </Grid>
-                    <Grid item xs={12} md={8}>
-                        <TextareaAutosize name="quote" style={{ width: '100%', height: '20vh', fontFamily: 'Montserrat', fontWeight: '600', borderRadius: '8px', padding: 10 }} aria-label="empty textarea" placeholder="Message..." />
-                    </Grid>
-                    <Grid item style={{ textAlign: 'end' }} xs={12} md={8}>
-                        <Button type="submit" variant="contained" size="large" style={{ width: '15%', color: '#3751FF', fontFamily: 'Montserrat', backgroundColor: '#3A86FF' }}>
-                            <Typography style={{ fontFamily: 'Montserrat', color: '#fff', fontSize: '3vmin', fontWeight: '600' }}>Send</Typography>
-                        </Button>
-                    </Grid>
-                </form>
+                        </Grid>
+                        <Grid item xs={12} md={4}>
+                            <input name="name" type="text" className={classes.textInput} placeholder="Enter your name" />
+                        </Grid>
+                        <Grid item xs={12} md={4}>
+                            <input name="email" type="email" className={classes.textInput} placeholder="Enter your email" />
+                        </Grid>
+                        <Grid item xs={12} md={8}>
+                            <TextareaAutosize name="quote" style={{ width: '100%', height: '20vh', fontFamily: 'Montserrat', fontWeight: '600', borderRadius: '8px', padding: 10 }} aria-label="empty textarea" placeholder="Message..." />
+                        </Grid>
+                        <Grid item style={{ textAlign: 'end' }} xs={12} md={8}>
+                            <Button type="submit" variant="contained" size="large" style={{ width: '15%', color: '#3751FF', fontFamily: 'Montserrat', backgroundColor: '#3A86FF' }}>
+                                <Typography style={{ fontFamily: 'Montserrat', color: '#fff', fontSize: '3vmin', fontWeight: '600' }}>Send</Typography>
+                            </Button>
+                        </Grid>
+                    </form>
+                </Grid>
                 <Grid item xs={12}>
                     <Typography className={classes.heading} variant="h3" gutterBottom>
                         Get in Touch
@@ -84,7 +81,6 @@ const ContactUs = () => {
                 </Grid>
                 <Grid style={{ textAlign: 'center' }} item xs={6} sm={6} md={6} lg={4}>
                     <Box style={{ display: 'block', textAlign: 'center', alignItems: 'center' }}>
-                        {/* <img alt="call" style={{width:'9vmin'}} src={callIcon} /> */}
                         <CallIcon />
                         <Box style={{ padding: '2vmin' }}>
                             <Typography className={classes.iconText}>
@@ -98,7 +94,6 @@ const ContactUs = () => {
                 </Grid>
                 <Grid style={{ textAlign: 'center' }} item xs={6} sm={6} md={6} lg={4}>
                     <Box style={{ display: 'block', textAlign: 'center', alignItems: 'center' }}>
-                        {/* <img alt="email" style={{width:'9vmin'}} src={emailIcon} /> */}
                         <EmailIcon />
                         <Box style={{ textAlign: 'center', padding: '2vmin' }}>
                             <Typography className={classes.iconText}>
@@ -112,7 +107,6 @@ const ContactUs = () => {
                 </Grid>
                 <Grid style={{ textAlign: 'center' }} item xs={8} sm={6} md={6} lg={4}>
                     <Box style={{ display: 'block', textAlign: 'center', alignItems: 'center' }}>
-                        {/* <img alt="location" style={{width:'9vmin'}} src={mapIcon} /> */}
                         <AddressIcon />
                         <Box style={{ padding: '2vmin' }}>
                             <Typography className={classes.iconText}>
@@ -124,32 +118,6 @@ const ContactUs = () => {
                         </Box>
                     </Box>
                 </Grid>
-                {/* <Grid item style={{textAlign:'center'}} xs={4} sm={6} md={6} lg={4}>
-            <Box style={{display:'block',width:'fit-content',  alignItems:'center', justifyContent:'space-evenly'}}>
-            <img alt="email" style={{width:'9vmin'}} src={emailIcon} />
-            <Box style={{padding:'2vmin'}}>
-            <Typography s className={classes.iconText}>
-            Email
-            </Typography>
-            <Typography s className={classes.iconText2} >
-            Hello@studio.co
-            </Typography>
-            </Box>
-                 </Box>
-         </Grid>
-            <Grid style={{textAlign:'center'}} item xs={4} sm={6} md={6} lg={4}>
-            <Box style={{display:'block',width:'fit-content',  alignItems:'center', justifyContent:'space-evenly'}}>
-            <img alt="location" style={{width:'9vmin'}} src={mapIcon} />
-            <Box style={{padding:'2vmin'}}>
-            <Typography  className={classes.iconText}>
-            Map Street
-            </Typography>
-            <Typography  className={classes.iconText2} >
-            John Bucarest st 100
-            </Typography>
-            </Box>
-                 </Box>
-      </Grid> */}
             </Grid>
         </div>
     );
