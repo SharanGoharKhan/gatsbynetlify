47bce2171114d6a0fc7cfe4a16b8647fc141c3ba
Sharan Khan <sharan.gohar@gmail.com>
2021-06-04 15:41:18 +0500

    form sub attempt

--- src/components/Contactus/index.js
+++ src/components/Contactus/index.js
@@ -1,6 +1,6 @@
 
-import React, { useState } from "react";
-import { makeStyles, Grid, Typography, TextareaAutosize,Button, Box } from '@material-ui/core';
+import React from "react";
+import { makeStyles, Grid, Typography, TextareaAutosize, Button, Box } from '@material-ui/core';
 import CallIcon from "../../svg/callIcon";
 import AddressIcon from "../../svg/addressIcon";
 import EmailIcon from "../../svg/emailIcon";
@@ -12,8 +12,6 @@ const useStyles = makeStyles((theme) => ({
         padding: theme.spacing(6),
         height: 'fit-content',
         textAlign: 'center',
-        // paddingLeft:theme.spacing(10),
-        // paddingRight:theme.spacing(10),
         marginTop: theme.spacing(6)
     },
     textInput: {
@@ -49,28 +47,29 @@ const useStyles = makeStyles((theme) => ({
 
 const ContactUs = () => {
     const classes = useStyles();
-    const [params, setParams] = useState({
-        username: '',
-        email: '',
-        message: ''
-    })
+    // const [params, setParams] = useState({
+    //     username: '',
+    //     email: '',
+    //     message: ''
+    // })
     return (
         <div id="contact" className={classes.container}>
             <Grid container justify="center" direction="row" spacing={5} alignItems="center" >
-                <form name="contact" action="POST" data-netlify="true">
+                <form name="contact" method="post" data-netlify="true" netlify-honeypot="bot-field">
+                    <input type="hidden" name="form-name" value="contact" />
                     <Grid item xs={12}>
                         <Typography className={classes.heading} variant="h3" gutterBottom>
                             Let's Talk with us
-                </Typography>
+                        </Typography>
                     </Grid>
                     <Grid item xs={12} md={4}>
-                        <input name="name" value={params.username} onChange={(event) => { setParams({ ...params, username: event.target.value }) }} type="text" className={classes.textInput} placeholder="Enter your name" />
+                        <input name="name" type="text" className={classes.textInput} placeholder="Enter your name" />
                     </Grid>
                     <Grid item xs={12} md={4}>
-                        <input name="email" type="text" onChange={(event) => { setParams({ ...params, email: event.target.value }) }} value={params.email} className={classes.textInput} placeholder="Enter your email" />
+                        <input name="email" type="text" className={classes.textInput} placeholder="Enter your email" />
                     </Grid>
                     <Grid item xs={12} md={8}>
-                        <TextareaAutosize name="quote" value={params.message} onChange={(event) => { setParams({ ...params, message: event.target.value }) }} style={{ width: '100%', height: '20vh', fontFamily: 'Montserrat', fontWeight: '600', borderRadius: '8px', padding: 10 }} aria-label="empty textarea" placeholder="Message..." />
+                        <TextareaAutosize name="quote" style={{ width: '100%', height: '20vh', fontFamily: 'Montserrat', fontWeight: '600', borderRadius: '8px', padding: 10 }} aria-label="empty textarea" placeholder="Message..." />
                     </Grid>
                     <Grid item style={{ textAlign: 'end' }} xs={12} md={8}>
                         <Button type="submit" variant="contained" size="large" style={{ width: '15%', color: '#3751FF', fontFamily: 'Montserrat', backgroundColor: '#3A86FF' }}>
@@ -81,47 +80,47 @@ const ContactUs = () => {
                 <Grid item xs={12}>
                     <Typography className={classes.heading} variant="h3" gutterBottom>
                         Get in Touch
-            </Typography>
-            </Grid>
-            <Grid style={{textAlign:'center'}} item xs={6} sm={6} md={6} lg={4}>
-            <Box style={{display:'block',textAlign:'center', alignItems:'center'}}>
-            {/* <img alt="call" style={{width:'9vmin'}} src={callIcon} /> */}
-            <CallIcon />
-            <Box style={{padding:'2vmin'}}> 
-            <Typography className={classes.iconText}>
-                Phone Number
-            </Typography>
+                    </Typography>
+                </Grid>
+                <Grid style={{ textAlign: 'center' }} item xs={6} sm={6} md={6} lg={4}>
+                    <Box style={{ display: 'block', textAlign: 'center', alignItems: 'center' }}>
+                        {/* <img alt="call" style={{width:'9vmin'}} src={callIcon} /> */}
+                        <CallIcon />
+                        <Box style={{ padding: '2vmin' }}>
+                            <Typography className={classes.iconText}>
+                                Phone Number
+                            </Typography>
                             <Typography className={classes.iconText2} >
                                 (62) 1829017
-            </Typography>
-            </Box>
-                 </Box>
-            </Grid>
-            <Grid style={{textAlign:'center'}} item xs={6} sm={6} md={6} lg={4}>
-            <Box style={{display:'block',textAlign:'center', alignItems:'center'}}>
-            {/* <img alt="email" style={{width:'9vmin'}} src={emailIcon} /> */}
-            <EmailIcon />
-            <Box style={{textAlign:'center',padding:'2vmin'}}> 
-            <Typography className={classes.iconText}>
-            Email
-            </Typography>
+                            </Typography>
+                        </Box>
+                    </Box>
+                </Grid>
+                <Grid style={{ textAlign: 'center' }} item xs={6} sm={6} md={6} lg={4}>
+                    <Box style={{ display: 'block', textAlign: 'center', alignItems: 'center' }}>
+                        {/* <img alt="email" style={{width:'9vmin'}} src={emailIcon} /> */}
+                        <EmailIcon />
+                        <Box style={{ textAlign: 'center', padding: '2vmin' }}>
+                            <Typography className={classes.iconText}>
+                                Email
+                            </Typography>
                             <Typography className={classes.iconText2} >
                                 Hello@studio.co
-            </Typography>
-            </Box>
-                 </Box>
-            </Grid>
-            <Grid style={{textAlign:'center'}} item xs={8} sm={6} md={6} lg={4}>
-            <Box style={{display:'block',textAlign:'center', alignItems:'center'}}>
-            {/* <img alt="location" style={{width:'9vmin'}} src={mapIcon} /> */}
-            <AddressIcon />
-            <Box style={{padding:'2vmin'}}> 
-            <Typography className={classes.iconText}>
-            Map Street
-            </Typography>
+                            </Typography>
+                        </Box>
+                    </Box>
+                </Grid>
+                <Grid style={{ textAlign: 'center' }} item xs={8} sm={6} md={6} lg={4}>
+                    <Box style={{ display: 'block', textAlign: 'center', alignItems: 'center' }}>
+                        {/* <img alt="location" style={{width:'9vmin'}} src={mapIcon} /> */}
+                        <AddressIcon />
+                        <Box style={{ padding: '2vmin' }}>
+                            <Typography className={classes.iconText}>
+                                Map Street
+                            </Typography>
                             <Typography className={classes.iconText2} >
                                 John Bucarest st 100
-            </Typography>
+                            </Typography>
                         </Box>
                     </Box>
                 </Grid>
