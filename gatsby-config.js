83f646f49aac68db1d03ba6654bba201e25bc1a9
Sharan Khan <sharan.gohar@gmail.com>
2021-06-16 11:03:54 +0500

    added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,13 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "UA-135271232-1" // Google Analytics / GA
+        ],
+      },
       resolve: `gatsby-plugin-material-ui`,
       options: {
         stylesProvider: {
