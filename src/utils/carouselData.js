c4728ab977c1d89ab10117ca126705c58db1b996
Sharan <sharan.gohar@gmail.com>
2022-04-15 01:12:48 +0500

    added testimonial

--- src/utils/carouselData.js
+++ src/utils/carouselData.js
@@ -23,6 +23,11 @@ export const carouselData = [
     review:
       "Thanks to Ninjas Code, the app was published in both the App Store and Google Play. The team communicated via Slack during the engagement. They delivered the project on time, meeting the client's expectations.",
   },
+  {
+    name: "Dan Gutting",
+    designation: "Co-founder",
+    review: `Sharan is an excellent communicator. I was able to chat with him about the project requirements and discuss all aspects of the work. There was no doubt that everything was understood. I can't overstate how beneficial this was. He is resourceful and helpful. I also found that he was able to produce responsive React Native and React Native for web pages that followed the mockups closely. He has an eye for this which was a real benefit to the project.`,
+  }
 ]
 
 export const smallCarouselData = [
